d274b7462d4d1b0a07ecae20e8f904b8cde21f9c
mizuho58128 <mizuho941@gmail.com>
2025-07-05 18:56:19 +0900

    Update HelloController.java

--- src/main/java/demo/controller/HelloController.java
+++ src/main/java/demo/controller/HelloController.java
@@ -2,6 +2,7 @@ package demo.controller; // ← パッケージ名を必ずディレクトリ構
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class HelloController {
@@ -9,5 +10,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello, Spring Boot!";
+    }
+
+    @GetMapping("/user/{name}")
+    public String username(@PathVariable String name) {
+        return "こんにちは、" + name + "さん！";
     }
 }
